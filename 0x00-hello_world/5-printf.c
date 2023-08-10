a83b31db452e39834bd0ef7946e82b4df1621404
kalid ebrahim <kalidibrahim2@gmail.com>
2023-08-10 02:59:31 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-display nessage on;screan
+ * Return: 0(succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
