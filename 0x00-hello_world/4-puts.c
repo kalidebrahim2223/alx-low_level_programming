da3641f6d35c7391a2f2feee2c3f6cccf5d4c9f5
kalid ebrahim <kalidibrahim2@gmail.com>
2023-08-10 02:49:12 -0700

    hellow world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entery point for the program
+ * Return: 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
