fb858569ee77becc792ed1d6f8c9a13eb30c229d
kalid ebrahim <kalidibrahim2@gmail.com>
2023-08-20 11:38:37 -0700

    upercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,33 +1,17 @@
 #include "main.h"
+
 /**
- *_isupper - separeat isupper or lower case
- * Return: 1 0r 0
- * @c:the value
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
  */
 
 int _isupper(int c)
 {
-	char i;
-	int r = 1;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (c == i)
-		{
-			r = 0;
-			break;
-		}
-	}
-	if (r == 1)
-	{
-		for (i = 'A'; i <= 'Z'; i++)
-		{
-			if (c == i)
-			{
-				r = 1;
-				break;
-			}
-		}
-	}
-	return (r);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
