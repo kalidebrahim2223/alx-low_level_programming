af68b4509acfe07c0c47136c8815c5548065625b
kalid ebrahim <kalidibrahim2@gmail.com>
2023-08-21 12:40:52 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: the value of r;
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
