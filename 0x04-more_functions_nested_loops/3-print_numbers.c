cca900f14389b7c3c9ec007ffe0dc4b8de6fd8ab
kalid ebrahim <kalidibrahim2@gmail.com>
2023-08-27 12:02:15 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
