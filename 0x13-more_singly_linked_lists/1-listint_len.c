faa611d80dd93f807a287990c8babea5ab09cd3a
kalid ebrahim <kalidibrahim2@gmail.com>
2023-09-25 12:32:25 -0700

    44

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
+
 /**
- *listint_len-count single linked list
- *@h:head
- *Return: number of eleements
+ * listint_len - the number of elements in a linked listint_t list.
+ *
+ * @h: head pointer
+ *
+ * Return: the number of elements
+ *
  */
+
 size_t listint_len(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
+	h = h->next;
 		count++;
 	}
 	return (count);
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,28 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- *add_nodeint- insert new element in first row
- *@head :pointer to the head
- *@n: intiger
- */
-
+  * add_nodeint - add node at beginning of a listint_t list.
+  *
+  * @head: head of double pointer
+  * @n: int add the list
+  * Return: NULL if it failed
+  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *ptr;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
 	if (head == NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-		*head = new;
-	}
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
 }
